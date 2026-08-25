ccabf45f47c39bc8ab584fad15624fcec7a51102
AmritKumar98 <amrit.kumar@amlgolabs.com>
2026-08-25 15:54:07 +0530

    token

--- src/app/context/assessments.context.js
+++ src/app/context/assessments.context.js
@@ -145,7 +145,7 @@ export const AppProvider = ({ children }) => {
     //   console.error("Failed to fetch assessments:", error);
 
     // }
-    const token = localStorage.getItem("token");
+    const token = getClientCookie("accessToken");
     console.log("Retrieved token from cookies:", token);
   if (!token) {
     console.error("No token found in cookies.");
