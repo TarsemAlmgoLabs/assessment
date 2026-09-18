4eb75dd52af7b6cbef16cdd8ab42aace3dec75ee
tarsem singh <Tarsem.Singh@amlgolabs.com>
2026-09-18 14:43:40 +0530

    fixing build

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -1,8 +1,18 @@
 
-import AssessmentResult from "../components/submission"
-export default function Result(){
-    return(
-        <AssessmentResult/>
-    )
-}
+// import AssessmentResult from "../components/submission"
+// export default function Result(){
+//     return(
+//         <AssessmentResult/>
+//     )
+// }
 
+import { Suspense } from "react";
+import AssessmentResult from "../components/submission";
+
+export default function Result() {
+  return (
+    <Suspense fallback={<div>Loading results...</div>}>
+      <AssessmentResult />
+    </Suspense>
+  );
+}
