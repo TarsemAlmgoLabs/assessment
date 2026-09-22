726af86272ac07076ec0ca3e511b7e810d997711
tarsem singh <Tarsem.Singh@amlgolabs.com>
2026-09-22 15:15:18 +0530

    fixing build

--- src/app/allResults/page.js
+++ src/app/allResults/page.js
@@ -1,6 +1,17 @@
-import TestResults from "../components/allResults"
-export default function Allresults(){
-    return(
-        <TestResults/>
-    )
+// import TestResults from "../components/allResults"
+// export default function Allresults(){
+//     return(
+//         <TestResults/>
+//     )
+// }
+
+import { Suspense } from "react";
+import TestResults from "../components/allResults";
+
+export default function Allresults() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <TestResults />
+    </Suspense>
+  );
 }
--- src/app/manage-assessment/page.js
+++ src/app/manage-assessment/page.js
@@ -1,7 +1,18 @@
-import ManageAssessment from "../components/manage"
+// import ManageAssessment from "../components/manage"
 
-export default function ManageAssessmentfun(){
-    return(
-        <ManageAssessment/>
-    )
+// export default function ManageAssessmentfun(){
+//     return(
+//         <ManageAssessment/>
+//     )
+// }
+
+import { Suspense } from "react";
+import ManageAssessment from "../components/manage";
+
+export default function ManageAssessmentfun() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ManageAssessment />
+    </Suspense>
+  );
 }
