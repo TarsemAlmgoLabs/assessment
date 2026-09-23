f5f920ca5938845b306afefb4e955cfb1114af03
tarsem singh <Tarsem.Singh@amlgolabs.com>
2026-09-23 11:54:56 +0530

    temperory redirect close

--- src/app/page.js
+++ src/app/page.js
@@ -74,10 +74,10 @@ export default function Home() {
   }
 
   // Authentication fail
-  if (authenticated === false) {
-    window.location.href = "https://vestaff.com/auth/login";
-    return null;
-  }
+  // if (authenticated === false) {
+  //   // window.location.href = "https://vestaff.com/auth/login";
+  //   return null;
+  // }
 
   return <CandidateAssessments />;
 }
